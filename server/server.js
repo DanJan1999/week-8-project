9a7133accb757b8a0f99397bc23f9ef566e65b46
DanJan1999 <danieljanis99@gmail.com>
2021-09-20 08:48:47 -0600

    First Commit after set-up

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require(`express`);
+const path = require(`path`);
+
+const app = express();
+
+app.get(`/`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../index.html`))
+});
+
+app.get(`/js`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../main.js`))
+}) ;
+
+app.get(`/css`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../styles.css`))
+});
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`Up and running on port ${port}`)
+});
