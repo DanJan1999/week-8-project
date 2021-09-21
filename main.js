c1c8a2af1551077a2435b9e0f27a1f95fe0c280b
DanJan1999 <danieljanis99@gmail.com>
2021-09-20 21:27:19 -0600

    Phase 2 partial complete. Awaiting repeat questions keeping original answers.

--- main.js
+++ main.js
@@ -8,41 +8,73 @@ const triangle = document.querySelector(`#message-display`);
 
 const baseUrl = `http://localhost:4005`
 
-const submitQ = (e) => {
 
-    if (inquiry && inquiry.value) {
+const submitQ = (e) => {
     e.preventDefault()
-    axios.get(`${baseUrl}/fortune`)
-    .then((res) => {
-        ball.classList.toggle(`ball`)
-        message.classList.toggle(`fade-out`)
-        triangle.classList.toggle(`fade-out`)
-        setTimeout(() => {
+    let inquirys = inquiry.value
+    let repeatedQuestions = []
+    const runGet = () => {
+        e.preventDefault()
+        axios.get(`${baseUrl}/fortune`)
+        .then((res) => {
+            ball.classList.toggle(`ball`)
             message.classList.toggle(`fade-out`)
             triangle.classList.toggle(`fade-out`)
-            message.classList.toggle(`fade-in`)
-            triangle.classList.toggle(`fade-in`)
-            message.textContent = res.data
-        }, 1000)
-        setTimeout(() => {
-            ball.classList.toggle(`ball`)
-            message.classList.toggle(`fade-in`)
-            triangle.classList.toggle(`fade-in`)
-        }, 5000)
-    })
-    inquiry.value= ``
-    } else {
-        alert(`Please type a question!`)
+            let repQ = { type:`${res.data.type}`, key:`${inquirys}`, value:`${res.data.value}` }
+            repeatedQuestions.push(repQ)
+            setTimeout(() => {
+                message.classList.toggle(`fade-out`)
+                triangle.classList.toggle(`fade-out`)
+                message.classList.toggle(`fade-in`)
+                triangle.classList.toggle(`fade-in`)
+                message.textContent = res.data.value
+            }, 1000)
+            setTimeout(() => {
+                ball.classList.toggle(`ball`)
+                message.classList.toggle(`fade-in`)
+                triangle.classList.toggle(`fade-in`)
+            }, 5000)
+        })
     }
+    console.log(inquirys)
+    console.log(repeatedQuestions)
+    if (repeatedQuestions.length === 0){
+        runGet()
+    } else if (repeatedQuestions.length >= 1){
+    for ( let i = 0; i < repeatedQuestions.length; i++) {
+        if (repeatedQuestions.contains(`positive`) || repeatedQuestions.contains(`negative`)) {
+            if (repeatedQuestions.contains(`${inquirys}`)) {
+                ball.classList.toggle(`ball`)
+                message.classList.toggle(`fade-out`)
+                triangle.classList.toggle(`fade-out`)
+                setTimeout(() => {
+                    message.classList.toggle(`fade-out`)
+                    triangle.classList.toggle(`fade-out`)
+                    message.classList.toggle(`fade-in`)
+                    triangle.classList.toggle(`fade-in`)
+                    message.textContent = repeatedQuestions[i].key
+                        }, 1000)
+                        setTimeout(() => {
+                            ball.classList.toggle(`ball`)
+                            message.classList.toggle(`fade-in`)
+                            triangle.classList.toggle(`fade-in`)
+                        }, 5000)
+                    } else {
+                        runGet()
+                    }
+                } else {
+                    runGet()
+                }
+             }
+        }
+        inquiry.value = ``
 };
 
 
 const submitHandler = () => {
-
     const submit = form.submit(submitQ)
     return submit
 };
-
 
 ball.addEventListener(`click`, submitQ);
 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const repeatedQs = []
 
 app.get(`/`, (req, res) => {
     res.sendFile(path.join(__dirname, `../index.html`))
@@ -14,33 +15,33 @@ app.get(`/`, (req, res) => {
 
 app.get(`/fortune`, (req, res) => {
     const fortunes = [
-        `It is certain.`,
-        `Without a doubt.`,
-        `You may rely on it.`,
-        `Yes definitely.`,
-        `It is decidedly so.`,
-        `As I see it, yes.`,
-        `Most likely.`,
-        `Yes.`,
-        `Outlook good.`,
-        `Signs point to yes.`,
-        `Reply hazy try again`,
-        `Better not tell you now.`,
-        `Ask again later.`,
-        `Cannot predict now.`,
-        `Concentrate and ask again.`,
-        `Don’t count on it.`,
-        `Outlook not so good.`,
-        `My sources say no.`,
-        `Very doubtful.`,
-        `My reply is no.`
+        {type:'positive', value:`It is certain.`},
+        {type:'positive', value:`Without a doubt.`},
+        {type:'positive', value:`You may rely on it.`},
+        {type:'positive', value:`Yes definitely.`},
+        {type:'positive', value:`It is decidedly so.`},
+        {type:'positive', value:`As I see it, yes.`},
+        {type:'positive', value:`Most likely.`},
+        {type:'positive', value:`Yes.`},
+        {type:'positive', value:`Outlook good.`},
+        {type:'positive', value:`Signs point to yes.`},
+        {type:'neutral', value:`Reply hazy try again`},
+        {type:'neutral', value:`Better not tell you now.`},
+        {type:'neutral', value:`Ask again later.`},
+        {type:'neutral', value:`Cannot predict now.`},
+        {type:'neutral', value:`Concentrate and ask again.`},
+        {type:'negative', value:`Don’t count on it.`},
+        {type:'negative', value:`Outlook not so good.`},
+        {type:'negative', value:`My sources say no.`},
+        {type:'negative', value:`Very doubtful.`},
+        {type:'negative', value:`My reply is no.`}
     ]
-
     let randomIndex = Math.floor(Math.random() * fortunes.length)
     let randomFortune = fortunes[randomIndex]
-    console.log(randomFortune)
     res.status(200).send(randomFortune)
 });
+
+ 
 
 app.get(`/js`, (req, res) => {
     res.sendFile(path.join(__dirname, `../main.js`))
